61466880df1c19027faa776d44b9a762fc915dae
Jannis Beichler | PAWI <jannis.beichler@pawi.com>
2024-03-25 21:23:08 +0100

    add Function loadTotalMoney and saveTotalMoney

--- app/src/main/java/com/example/dicelucky/GameDataHandler.java
+++ app/src/main/java/com/example/dicelucky/GameDataHandler.java
@@ -9,5 +9,14 @@ public class GameDataHandler {
     private static final String PREF_NAME = "com.example.dicelucky.GameDataHandler";
     private static final String KEY_MONEY = "money";
 
-  
+    public static int loadTotalMoney(Activity activity) {
+        SharedPreferences prefs = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return prefs.getInt(KEY_MONEY, 100); // Default start Coins: 100
+    }
+
+    public static void saveTotalMoney(Activity activity, int totalMoney) {
+        SharedPreferences.Editor editor = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit();
+        editor.putInt(KEY_MONEY, totalMoney);
+        editor.apply();
+    }
 }
