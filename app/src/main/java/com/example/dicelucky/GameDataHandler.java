6a67d68de3598a98b4f5f0d8cec82d2b7dbd6579
Jannis Beichler | PAWI <jannis.beichler@pawi.com>
2024-03-25 21:22:08 +0100

    Add Class GameDataHandler

---
+++ app/src/main/java/com/example/dicelucky/GameDataHandler.java
@@ -0,0 +1,13 @@
+package com.example.dicelucky;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class GameDataHandler {
+
+    private static final String PREF_NAME = "com.example.dicelucky.GameDataHandler";
+    private static final String KEY_MONEY = "money";
+
+  
+}
